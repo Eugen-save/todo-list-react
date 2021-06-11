e3e4385f3e7a06dc4b9250e14e1eb039aabd8a39
Evgeny Savelyev <savelyev.eugeny@gmail.com>
2021-06-11 06:06:02 +0200

    set all done interaction

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import "./style.css";
 
-const Buttons = ({ tasks, hideDone, toggleHideDone }) => (
+const Buttons = ({ tasks, hideDone, toggleHideDone, setAllDone }) => (
     <div className="section__button">
         {tasks.length > 0 && (
             <React.Fragment>
                 <button onClick={toggleHideDone} className="section__button">
                     {hideDone ? "Pokaż" : "Ukryj"} ukończone
     </button>
-                <button className="section__button"
+                <button
+                    className="section__button"
+                    onClick={setAllDone}
                     disabled={tasks.every(({ done }) => done)}>
                     Ukończ wszystkie
     </button>
