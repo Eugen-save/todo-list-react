691cfbe5446a06428a4a1d3d413c5ad54b3afaa5
Evgeny Savelyev <savelyev.eugeny@gmail.com>
2021-07-14 07:13:06 +0200

    saving tasks in local storage added

---
+++ src/features/tasks/tasksSaga.js
@@ -0,0 +1,23 @@
+import { takeEvery, call, put, select } from "redux-saga/effects";
+import { getExampleTasks } from "./getExampleTasks";
+import { saveTasksInLocalStorage } from "./tasksLocalStorage";
+import { fetchExampleTasks, selectTasks, setTasks } from "./tasksSlice";
+
+function* fetchExampleTasksHandler() {
+    try {
+        const exampleTasks = yield call(getExampleTasks);
+        yield put(setTasks(exampleTasks))
+    } catch (error) {
+        yield call(alert, "Coś poszło nie tak!");
+    }
+}
+
+function* saveTasksInLocalStorageHandler() {
+    const tasks = yield select(selectTasks);
+    yield call(saveTasksInLocalStorage, tasks);
+}
+
+export function* tasksSaga() {
+    yield takeEvery(fetchExampleTasks.type, fetchExampleTasksHandler);
+    yield takeEvery("*", saveTasksInLocalStorageHandler)
+}
